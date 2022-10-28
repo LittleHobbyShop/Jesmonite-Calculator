52c236d75c2bb398079f4701a8e99bf83324774e
Duncan Band <band.duncan@gmail.com>
2022-10-28 19:50:07 +0100

    add materials

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import materials from "./resources/materials";
 
-function App() {
+const App = () => {
+  const [material, setMaterial] = useState(0);
+  const [mass, setMass] = useState(0);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMaterial(parseInt(event.target.value));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <FormControl>
+        <FormLabel id="demo-row-radio-buttons-group-label">Material</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="demo-row-radio-buttons-group-label"
+          name="row-radio-buttons-group"
+          value={material}
+          onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
+          {materials.bases.map(({ name }, i) => {
+            return (
+              <FormControlLabel value={i} control={<Radio />} label={name} />
+            );
+          })}
+        </RadioGroup>
+      </FormControl>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/resources/materials.ts
@@ -0,0 +1,13 @@
+const materials = {
+  bases: [
+    { name: "AC100", density: 1750 },
+    { name: "AC730", density: 1950 },
+  ],
+  pigments: [
+    { name: "White", density: 25, max: 0.02 },
+    { name: "Black", density: 50, max: 0.02 },
+  ],
+  additives: [{ name: "retarder", max: 0.06 }],
+};
+
+export default materials;
